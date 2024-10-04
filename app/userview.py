9556b3f79882f7afd82694bbd59f8a0de08e4932
FIRST_NAME LAST_NAME <charantm8787@gmail.com>
2024-10-04 13:48:28 +0530

    .

--- app/userview.py
+++ app/userview.py
@@ -5,3 +5,7 @@ user_interface = Blueprint('user_interface',__name__)
 @user_interface.route('/')
 def home():
     return render_template('home.html')
+
+@user_interface.route('/raisecomplaint')
+def raisecomplaint():
+    return render_template('raise_complaint.html')
